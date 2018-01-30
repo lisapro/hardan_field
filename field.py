495e5d2aaac466a10e5f3bcf3e481be100f54a7b
lisapro <elisaveta.pro@gmail.com>
2018-01-30 15:35:11 +0100

    new grid

--- field.py
+++ field.py
@@ -4,6 +4,8 @@ Created on 19. july 2017
 @author: ELP
 '''
 
+# ---------- LOAD LIBRARIES ------------  
+
 import os,sys
 import netCDF4
 from netCDF4 import Dataset
@@ -11,23 +13,37 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
-import Tkinter as tk
+try:
+    import Tkinter as tk
+    from tkFileDialog import askopenfilename # python 2  
+except ModuleNotFoundError :
+    import tkinter as tk # python3 
+    from tkinter.filedialog import askopenfilename # for python 3
 
-from Tkinter import *
-from tkFileDialog import askopenfilename # python 2
 root = tk.Tk()
 root.withdraw()
-#from tkinter import * python3 
-#from tkinter.tkFiledialog import askopenfilename # for python 3
- 
 import time
 
-#plt.style.use('ggplot')
-plt.style.use('bmh')
+# ---------- DEFINE PLOT STYLE ------------  
 
-#[u'dark_background', u'bmh', u'grayscale', u'ggplot', u'fivethirtyeight']
-#3demo('default')
-#demo('seaborn')
+plt.style.use('bmh') # define plot style from the list of available: 
+# available (checked):'bmh','fivethirtyeight','ggplot'
+# not-available:'default', 'grayscale','seaborn-ticks','classic'
+# not-available:'seaborn-bright','default','dark_background','seaborn'
+# fte_graph(plt.backgroundcolor = 'red')
+
+# -- define fonts
+SMALL_SIZE  = 8
+MEDIUM_SIZE = 9
+BIGGER_SIZE = 12 #9
+plt.rc('font',    size = MEDIUM_SIZE)   # controls default text sizes
+plt.rc('axes', labelsize = BIGGER_SIZE)   # fontsize of the x and y labels
+
+plt.rc('xtick', labelsize = BIGGER_SIZE)   # fontsize of the tick labels
+plt.rc('ytick', labelsize = BIGGER_SIZE)   # fontsize of the tick labels
+plt.rc('legend', fontsize = SMALL_SIZE)   # legend fontsize
+
+# ---------- READ FILEs WITH OBSERVARION DATA ------------  
 
 with open('wat_hardan2.txt', 'r') as f:
     # important to specify delimiter right 
@@ -40,9 +56,9 @@ with open('wat_hardan2.txt', 'r') as f:
         #break
         r.append(row)        
 r1 = np.transpose(np.array(r[1:])) 
-#print (r1[0])
+
 st,nh4  = r1[0], r1[1]    
-#nh4 = r1[1]  
+
 po4 = r1[2]
 ptot = r1[3]
 ntot = r1[4]
@@ -63,8 +79,7 @@ alk = [2301.98,2180.317, 1059.57, 1058.218]
 start_list = [0,7,12,17,23,28] 
 end_list = [7,12,17,23,28,33]
 st_list = [1,2,3,4,5,6]
-
-#Cruise data from sediments 
+#Observations data from sediments 
 depth200 = [0.50, 1.20, 2.50 , 4.00, 7.5]
 depth199 = [0.50, 1.50, 2.50 , 4.00, 7.5]
 
@@ -82,17 +97,19 @@ alk199 = [3070, 3050, 3590, 4040, 3800]
 tic199 = [3141.7, 3641.7, 4191.7, 4133.3, 3916.7]
 nh4199 = [100.,114.2857143,145.7142857,107.1428571,92.85714286]
 
+# ---------- LOAD FILE WITH MODEL DATA (NetCDF) ------------  
 
-ask_filename = askopenfilename(
+'''ask_filename = askopenfilename(
     initialdir= os.getcwd(),
     filetypes =(("NetCDF file", "*.nc"),
                 ("All Files","*.*")),
-                title = "Choose a file.")
+                title = "Choose a needed  file.")
   
-fname = ask_filename # "BROM_Hfjord_out.nc"
+fname = ask_filename # "BROM_Hfjord_out.nc"'''
+fname = r'e:\Users\EYA\Hardangerfjorden\data_Hardangerfjord\plot_1D\BROM_Hfjord_out.nc'
 #os.path.split(ask_filename)[1] 
 
-fh =  Dataset(fname)
+fh =  Dataset(fname) # data from NetCDF will be in "fh" from now 
 
 kz =  np.array(fh.variables['Kz'][:,:,0])
 depth_brom = np.array(fh.variables['z'][:])
@@ -118,20 +135,22 @@ len_i = len(i_brom)
 #ax = figure1.add_subplot(gs[0])
 #ax1 = figure1.add_subplot(gs[1])
 
+# ---------- SUBROUTINES TO PLOT ------------ 
 
-
-# plot cruise data water  
-def plot_water(var,axis):
-    if var == alk or var == tic: 
+# plot Observations data water  
+def plot_water(var,axis,title):
+    if title == r'$ DIC\ \mu  M $' : # 'DIC': #var == alk or var == tic: 
         axis.scatter(var,tic_depth,color = '#825572',
                      zorder=10,edgecolors='k')
-        pass
+    elif title == r'$ Alk\ \mu  M $' :
+        axis.scatter(var,tic_depth,color = '#825572',
+                     zorder=10,edgecolors='k')           
     else: 
         for m in range(0,6):        
             axis.plot(var[start_list[m]:end_list[m]], 
                     pressure[start_list[m]:end_list[m]],
-                    'o-', color = '#9f6b8c')  #,
-                    #'o-',alpha = 0.7)        
+                    'o-', color = '#9f6b8c',
+                    alpha = 0.7)        
             #axis.annotate(st_list[m],
             #(var[start_list[m]],pressure[start_list[m]]),
             #              textcoords='offset points', 
@@ -141,68 +160,130 @@ def plot_water(var,axis):
             #             c = '#8b0000',s = 300,
             #             zorder = 10)        
                  
+# plot Observations data sediments  
 def plot_sed(var,var_depth,axis): 
     if var == None :
         pass
     else:    
         axis.plot(var,var_depth,'o-')
-        
+
+# plot model data water          
 def plot_brom(var_brom,axis):    
-    for day in range(0,len_time,20):  # plot every 20'th day  
-        for n in range(0,len_i,2): # plot every 2d column
+    for day in range(0,1370,10):  # plot every 10'th day  to day 1350 (can be "len_time")
+        if  day>1290 and day<1370 :
+            col = '#ff8800'
+        else :
+            col = '#a1a7af'
+
+        for n in range(0,10,1): # plot every 1st column in 10 column to avoid zone affected by FF
+#       for n in range(0,len_i,2): # plot every 2d column
                 #for m in range(0,100):# time_brom-1:
             axis.plot(var_brom[day,:15,n],depth_brom[:15],
-                      color = '#a1a7af',alpha = 0.8,
-                      linewidth = 0.2) 
-                   
+                      color =col,alpha = 0.6, # "alpha" is transperancy
+                      linewidth = 0.4) 
+
+# plot model data sediments                             
 def plot_brom_sed(var_brom,axis):  
-    for day in range(0,len_time,20):  
+    for day in range(0,1370,20):  
+        if  day>1290 and day<1370 :
+            col = '#ff8800'
+        else :
+            col = '#a1a7af'
         for n in range(0,len_i,2):
             axis.plot(var_brom[day,13:,n],sed_depth_brom[13:],
-                      color = '#a1a7af',alpha = 0.3,
-                      linewidth = 0.2,zorder = 1 )          
+                      color = col,alpha = 0.3,
+                      linewidth = 0.3,zorder = 1 ) 
+            
+                     
+figure1 = plt.figure(figsize=(8.27, 11.69), dpi=150) #figure(figsize=(5,8), dpi= 100)
+figure1.set_facecolor('white') # specify background color
+gs = gridspec.GridSpec(4,3)   # define 2 subplots, (i.e. y=2,x=1)
+gs.update(left = 0.1,right = 0.97, 
+          bottom = 0.07, top = 0.95,
+          wspace = 0.25, hspace= 0.25)         # shift figure from left boundary
 
-def plot_all(var_brom,var_water,var_sed1,var_sed2,title):    
-    #ax1.cla()
-    figure1 = plt.figure(figsize=(5,7), dpi=100)
-    gs = gridspec.GridSpec(2,1) #yx
 
-    ax = figure1.add_subplot(gs[0])
-    ax1 = figure1.add_subplot(gs[1])
-    ax.set_ylim(310,0)
-    ax1.set_ylim(5,-5) 
+ax = figure1.add_subplot(gs[0,0]) 
+ax.set_ylabel('Depth, m')
+
+ax1 = figure1.add_subplot(gs[1,0])
+ax1.set_ylabel('Depth, cm')
+
+ax2 = figure1.add_subplot(gs[0,1]) 
+ax3 = figure1.add_subplot(gs[1,1])
+
+ax4 = figure1.add_subplot(gs[0,2]) 
+ax5 = figure1.add_subplot(gs[1,2])
+
+ax6 = figure1.add_subplot(gs[2,0])
+ax6.set_ylabel('Depth, m')
+
+ax7 = figure1.add_subplot(gs[3,0]) 
+ax7.set_ylabel('Depth, cm')
+
+ax8 = figure1.add_subplot(gs[2,1])
+ax9 = figure1.add_subplot(gs[3,1])
+
+ax10 = figure1.add_subplot(gs[2,2])
+ax11 = figure1.add_subplot(gs[3,2])
+
+
+letters = ['(A)','(B)','(C)','(D)','(E)','(F)']
+
+n = 0
+for axis in (ax1,ax3,ax5,ax7,ax9,ax11): 
+    #axis.yaxis.set_label_coords(-0.1, 0.6)
+    axis.text(-0.25, -0.15, letters[n], transform=axis.transAxes , 
+            size = BIGGER_SIZE) #, weight='bold')      
+    n=n+1
+
+
+# plot figure with observarions and model for water and sediment     
+def plot_all(var_brom,var_water,var_sed1,var_sed2,title,axis,axis1):    
+
     
-    ax.axhspan(310, 0,color='#cce6f5',
+
+    axis.set_ylim(310,0)
+    axis1.set_ylim(3.1,-3.1)  #   axis1.set_ylim(5,-5) 
+    water_ticks = np.arange(0, 310, 100)                                             
+    swi_ticks = np.arange(-3, 3, 1.5)   
+    axis.set_yticks(water_ticks)     
+    axis1.set_yticks(swi_ticks)     
+   
+    axis.axhspan(310, 0,color='#cce6f5',
                  alpha = 0.4, label = "water")     
-    ax1.axhspan(0, -5,color='#cce6f5',
+    axis1.axhspan(0, -5,color='#cce6f5',
                  alpha = 0.4, label = "water")    
-    ax1.axhspan(5, 0,color='#dcc196',
+    axis1.axhspan(5, 0,color='#dcc196',
                  alpha = 0.4, label = "sediment")        
      
-    #plt.gcf().clear()
-    plot_brom(var_brom,ax)
-    plot_brom_sed(var_brom,ax1)            
-    plot_water(var_water,ax)
-    plot_sed(var_sed1,depth200,ax1)
-    plot_sed(var_sed2,depth199,ax1)
-    ax.set_title(title)
-    script_dir = os.path.dirname(__file__)
+    plot_brom(var_brom,axis)
+    plot_brom_sed(var_brom,axis1)            
+    plot_water(var_water,axis,title)
+    plot_sed(var_sed1,depth200,axis1)
+    plot_sed(var_sed2,depth199,axis1)
+    axis.set_title(title)
     
-    results_dir = os.path.join(script_dir, 'Results/')
+# ------------- FINALLY PLOT ! ------------
+plot_all(o2_brom,o2,None,None,r'$ O_2\ \mu  M $',ax,ax1) 
+plot_all(po4_brom,po4,po4199,po4200,r'$ PO_4\ \mu  M $',ax2,ax3)
+plot_all(no3_brom,no3,no3199,no3200,r'$ NO_3\ \mu  M $',ax4,ax5)
+
+plot_all(nh4_brom,nh4,nh4199,nh4200,r'$ NH_4\ \mu  M $',ax6,ax7)
+plot_all(alk_brom,alk,alk199,alk200,r'$ Alk\ \mu  M $',ax8,ax9)  
+plot_all(tic_brom,tic,tic199,tic200,r'$ DIC\ \mu  M $',ax10,ax11)
+
+
+
+
+script_dir = os.path.dirname(__file__)
+results_dir = os.path.join(script_dir, 'Results/')    
+if not os.path.isdir(results_dir):
+    os.makedirs(results_dir)
     
-    if not os.path.isdir(results_dir):
-       os.makedirs(results_dir)
-       
-    plt.savefig(results_dir+title+'.png')
-    #plt.show()
-    plt.clf()
-    
-# plt.savefig("/home/dir/subdir/filename.png")   
-plot_all(alk_brom,alk,alk199,alk200,'Alkalinity')  
-plot_all(si_brom,si,si199,si200,'Si')
-plot_all(no3_brom,no3,no3199,no3200,'NO3')
-plot_all(po4_brom,po4,po4199,po4200,'PO4')
-plot_all(o2_brom,o2,None,None,'O2')
-  
-plot_all(tic_brom,tic,tic199,tic200,'TIC')
-plot_all(nh4_brom,nh4,nh4199,nh4200,'NH4')
+plt.savefig(results_dir+'Figure5'+'.eps',
+            facecolor=figure1.get_facecolor(),
+            edgecolor='none')
+#plt.show()
+#plt.clf()
+#print ('ferdig!')
