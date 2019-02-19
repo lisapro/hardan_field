6e337ba6df689f11c5291089745c87fedf38570d
lisapro <elisaveta.pro@gmail.com>
2019-02-19 14:17:35 +0100

    add o2 field data, change styles

--- Figure1.py
+++ Figure1.py
@@ -4,7 +4,6 @@ Created on 19. july 2017
 @author: ELP
 '''
 
-# ---------- LOAD LIBRARIES ------------  
 
 import os,sys
 import netCDF4
@@ -13,13 +12,11 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
-import tkinter as tk 
-from tkinter.filedialog import askopenfilename 
-root = tk.Tk()
-root.withdraw()
+import pandas as pd
 import time
+import xarray as xr 
+from scipy import interpolate
 
-# ---------- DEFINE PLOT STYLE ------------  
 
 plt.style.use('bmh') 
 
@@ -34,29 +31,8 @@ plt.rc('xtick', labelsize = BIGGER_SIZE)   # fontsize of the tick labels
 plt.rc('ytick', labelsize = BIGGER_SIZE)   # fontsize of the tick labels
 plt.rc('legend', fontsize = SMALL_SIZE)   # legend fontsize
 
-# ---------- READ FILEs WITH OBSERVARION DATA ------------  
 
-with open('data/wat_hardan2.txt', 'r') as f:
-    # important to specify delimiter right 
-    reader = csv.reader(f, delimiter=',')
-    r = []
-    for row in reader:
-        r.append(row)        
-r1 = np.transpose(np.array(r[1:])) 
-
-st,nh4  = r1[0], r1[1]    
-po4 = r1[2]
-ptot = r1[3]
-ntot = r1[4]
-si = r1[5]  
-no3 = r1[6]  
-o2 = r1[7]  
-doc = r1[8]  
-pressure = r1[9]  
-fe3 = r1[10]  
-mn2 = r1[11]  
-dop = r1[12]  
-don = r1[13] 
+df = pd.read_csv('data/wat_hardan2.txt')
 
 #Observations
 tic = [2190.56, 2021.71, 988.85, 999.57]
@@ -85,86 +61,120 @@ alk199 = [3070, 3050, 3590, 4040, 3800]
 tic199 = [3141.7, 3641.7, 4191.7, 4133.3, 3916.7]
 nh4199 = [100.,114.2857143,145.7142857,107.1428571,92.85714286]
 
-# ---------- LOAD FILE WITH MODEL DATA (NetCDF) ------------  
 
 fname = (
-    r'E:\Users\EYA\Hardnew\data_Hard\BROM_Hardangerfjord_out.nc')
+     r'E:\Users\EYA\Hardnew\data_Hard\BROM_Hardangerfjord_out_1X.nc')
 
-fh =  Dataset(fname) 
 
-kz =  np.array(fh.variables['Kz'][:,:,0])
-depth_brom = np.array(fh.variables['z'][:])
-sed = np.array(fh.variables['z'][15]) # depth of the SWI
-sed_depth_brom = np.array(fh.variables['z'][:])-sed
-sed_depth_brom = sed_depth_brom *100
-si_brom = np.array(fh.variables['Si'][:,:,:]) 
-nh4_brom = np.array(fh.variables['NH4'][:,:,:])
-po4_brom = np.array(fh.variables['PO4'][:,:,:])
-no3_brom = np.array(fh.variables['NO3'][:,:,:])
-o2_brom = np.array(fh.variables['O2'][:,:,:])
-alk_brom = np.array(fh.variables['Alk'][:,:,:])
-tic_brom = np.array(fh.variables['DIC'][:,:,:])
-time_brom = np.array(fh.variables["time"][:])
+colr1 = '#ff8800'
+colr_base = '#2e3236'
+colr_base_field = '#519344'
+colr_farm = '#c75641'
+baseline_col = 0
+farm_col = 19
+a = 1
+ln  = 0.4
+ln_field = 1
 
+
+
+df_brom = xr.open_dataset(fname)
+
+depth_brom = df_brom['z'].values
+sed = depth_brom[15]
+sed_depth_brom  = (depth_brom - sed )*100
+time_brom = df_brom['time'].values
 len_time = len(time_brom)
-i_brom = np.array(fh.variables['i'][:]) 
+i_brom = df_brom['i'].values
 len_i = len(i_brom)
 
+# Add oxygen 
 
-# ---------- SUBROUTINES TO PLOT ------------ 
+def get_df(path):
+    d = pd.read_excel(path,skiprows = 1,usecols = [1,4,7],
+                      names = ['depth','time','o2'])
+    d.depth = d.depth / -10000
+    #d = d.resample('30s', on='time').mean()  
+    d.o2 = d['o2'].where(d.o2 >= 0, 0) 
+    return d
+    
+df_o2 = get_df('data/Jellyfarm/AKS193_1_FF.xlsx')
+df_o2_nf = get_df('data/Jellyfarm/AKS192_1_NF.xlsx')
+df_o2_2_nf = get_df('data/Jellyfarm/AKS189_3_NF.xlsx')
+df_o2_2 = get_df('data/Jellyfarm/AKS193_2_FF.xlsx')
+
+
+def int_and_plot(axis,xx,yy,col,col_line):
+
+    f = interpolate.interp1d(yy,xx, assume_sorted = False)
+    ynew = np.arange(np.min(yy),np.max(yy),0.3)
+    xnew = f(ynew) 
+    
+    
+    axis.plot(xnew,ynew, zorder =5, linestyle= '--',# s = 45,
+                     c = col_line,linewidth = ln_field,
+                     alpha = 1,label = 'field "Near Farm"')
+    axis.scatter(xnew,ynew, zorder = 10, s = 36, 
+                     c = col,edgecolor = 'k',
+                     alpha = 1,label = 'field "Near Farm"')    
+
+
+
+
 
 # plot Observations data water  
 def plot_water(var,axis,title):
-    if title == r'$ DIC\ \mu  M $' : # 'DIC': #var == alk or var == tic: 
-        axis.scatter(var,tic_depth,color = '#825572',
+    if title == r'$ DIC\ \mu  M $' :
+        axis.scatter(var,tic_depth,color = colr_base_field,
                      zorder=10,edgecolors='k')
     elif title == r'$ Alk\ \mu  M $' :
-        axis.scatter(var,tic_depth,color = '#825572',
+        axis.scatter(var,tic_depth,color = colr_base_field,
                      zorder=10,edgecolors='k')           
     else: 
         for m in range(0,6):        
             axis.plot(var[start_list[m]:end_list[m]], 
-                    pressure[start_list[m]:end_list[m]],
-                    'o-', color = '#9f6b8c',
-                    alpha = 0.7)        
-                     
+                    df.Pressure[start_list[m]:end_list[m]],
+                    'o', color = colr_base_field, markeredgecolor ='k',zorder = 10,  
+                    alpha = a)        
+            #axis.plot(var[start_list[m]:end_list[m]], 
+            #        df.Pressure[start_list[m]:end_list[m]],
+            #        '--', color = colr_base_field, zorder = 5, linewidth = ln_field, 
+            #        alpha = a)   
+
+
 # plot Observations data sediments  
 def plot_sed(var,var_depth,axis): 
     if var == None :
         pass
     else:    
-        axis.plot(var,var_depth,'o-')
+        axis.plot(var,var_depth,'--',color = colr_base,
+        linewidth = ln_field)
+        axis.plot(var,var_depth,'o',color = colr_base_field,
+        markeredgecolor ='k')
 
 # plot model data water          
 def plot_brom(var_brom,axis):    
-    for day in range(0,1370,30):  
-        # plot every 10'th day  to day 1350 (can be "len_time")
-        if  day>1290 and day<1370 :
-            col = '#ff8800'
-        else :
-            col = '#a1a7af'
-            
-        for n in range(0,10,2): 
-            #plot every 1st column in 10 column to avoid zone affected by FF
-#       for n in range(0,len_i,2): # plot every 2d column
-                #for m in range(0,100):# time_brom-1:
-            axis.plot(var_brom[day,:15,n],depth_brom[:15],
-                      color =col,alpha = 0.6, 
-                      linewidth = 0.4) 
+    v = df_brom[var_brom].values
+    for day in range(731,1096,10):  
+        axis.plot(v[day,:15,baseline_col],depth_brom[:15],
+                      color = colr_base, alpha = a, 
+                      linewidth = ln) 
+        axis.plot(v[day,:15,farm_col],depth_brom[:15],
+                      color = colr_farm, alpha = a, 
+                      linewidth = ln) 
 
 # plot model data sediments                             
-def plot_brom_sed(var_brom,axis):  
-    for day in range(0,1370,20):  
-        if  day>1290 and day<1370 :
-            col = '#ff8800'
-        else :
-            col = '#a1a7af'
-        for n in range(0,len_i,2):
-            axis.plot(var_brom[day,13:,n],sed_depth_brom[13:],
-                      color = col,alpha = 0.3,
-                      linewidth = 0.3,zorder = 1 ) 
-                              
-fig1 = plt.figure(figsize=(11.69, 8.27), dpi=140) 
+def plot_brom_sed(var_brom,axis): 
+    v = df_brom[var_brom].values 
+    for day in range(731,1096,10):  
+        axis.plot(v[day,13:,baseline_col],sed_depth_brom[13:],
+                      color = colr_base, alpha = a,
+                      linewidth = ln,zorder = 1 ) 
+        axis.plot(v[day,13:,farm_col],sed_depth_brom[13:],
+                      color = colr_farm, alpha = a,
+                      linewidth = ln,zorder = 1 ) 
+
+fig1 = plt.figure(figsize=(11.69, 8.27), dpi=100) 
 fig1.set_facecolor('white') # specify background color
 
 gs = gridspec.GridSpec(2, 6)
@@ -172,7 +182,6 @@ gs = gridspec.GridSpec(2, 6)
 gs.update(left = 0.07,right = 0.99, 
           bottom = 0.07, top = 0.95,
           wspace = 0.3, hspace= 0.15)
-
 
 ax = fig1.add_subplot(gs[0,0]) 
 ax1 = fig1.add_subplot(gs[1,0])
@@ -200,13 +209,14 @@ axes = (ax,ax2,ax4,ax6,ax8,ax10)
 
 [axes[n].text(-0.1, 1.05,letters[n], transform=axes[n].transAxes , 
             size = BIGGER_SIZE) for n in range(0,len(letters))]
-''''n = 0
-for axis in (ax1,ax3,ax5,ax7,ax9,ax11): 
-    #axis.yaxis.set_label_coords(-0.1, 0.6)
-    axis.text(-0.2, -0.12, letters[n], transform=axis.transAxes , 
-            size = BIGGER_SIZE) #, weight='bold')      
-    n=n+1'''
-
+'''
+#n = 0
+#for axis in (ax1,ax3,ax5,ax7,ax9,ax11): 
+#    #axis.yaxis.set_label_coords(-0.1, 0.6)
+#    axis.text(-0.2, -0.12, letters[n], transform=axis.transAxes , 
+#            size = BIGGER_SIZE) #, weight='bold')      
+#    n=n+1
+'''
 
 # plot figure with observations and model for water and sediment     
 def plot_all(var_brom,var_water,var_sed1,var_sed2,title,axis,axis1):    
@@ -232,24 +242,32 @@ def plot_all(var_brom,var_water,var_sed1,var_sed2,title,axis,axis1):
     plot_sed(var_sed2,depth199,axis1)
     axis.set_title(title)
     
-# ------------- FINALLY PLOT ! ------------
 
-plot_all(o2_brom,o2,None,None,r'$ O_2\ \mu  M $',ax,ax1) 
-plot_all(po4_brom,po4,po4199,po4200,r'$ PO_4\ \mu  M $',ax2,ax3)
-plot_all(no3_brom,no3,no3199,no3200,r'$ NO_3\ \mu  M $',ax4,ax5)
-plot_all(nh4_brom,nh4,nh4199,nh4200,r'$ NH_4\ \mu  M $',ax6,ax7)
-plot_all(alk_brom,alk,alk199,alk200,r'$ Alk\ \mu  M $',ax8,ax9)  
-plot_all(tic_brom,tic,tic199,tic200,r'$ DIC\ \mu  M $',ax10,ax11)
+plot_all('O2',df.O2uM,None,None,r'$ O_2\ \mu  M $',ax,ax1) 
+plot_all('PO4',df.PO4uM,po4199,po4200,r'$ PO_4\ \mu  M $',ax2,ax3)
+plot_all('NO3',df.NO3uM,no3199,no3200,r'$ NO_3\ \mu  M $',ax4,ax5)
+plot_all('NH4',df.NH4uM,nh4199,nh4200,r'$ NH_4\ \mu  M $',ax6,ax7)
+plot_all('Alk',alk,alk199,alk200,r'$ Alk\ \mu  M $',ax8,ax9)  
+plot_all('DIC',tic,tic199,tic200,r'$ DIC\ \mu  M $',ax10,ax11)
+
+int_and_plot(ax1,df_o2_2.o2,df_o2_2.depth,colr_farm,colr_farm)    
+int_and_plot(ax1,df_o2.o2,df_o2.depth,colr_farm,colr_farm) 
+int_and_plot(ax1,df_o2_nf.o2,df_o2_nf.depth,colr_base_field,colr_base_field) 
+int_and_plot(ax1,df_o2_2_nf.o2,df_o2_2_nf.depth,colr_base_field,colr_base_field) 
+
 
 
 script_dir = os.path.dirname(__file__)
 results_dir = os.path.join(script_dir, 'Results/')    
+
+save = False 
 if not os.path.isdir(results_dir):
     os.makedirs(results_dir)
-    
-plt.savefig(results_dir+'Figure1'+'.png', #'.eps'
-           facecolor=fig1.get_facecolor(),
-            edgecolor='none')
-#plt.show()
-#plt.clf()
-#print ('ferdig!')
+
+if save == True:    
+    plt.savefig(results_dir+'Figure1'+'.png', #'.eps'
+               facecolor=fig1.get_facecolor(),
+                edgecolor='none')
+else: 
+    plt.show()
+
