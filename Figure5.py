57c3afd6fe393eabcd16772ccec5fb8edf0019d6
lisapro <elisaveta.pro@gmail.com>
2019-02-22 11:46:01 +0100

    fixed size, styles in fig5

--- Figure5.py
+++ Figure5.py
@@ -4,32 +4,44 @@ Created on 19. july 2017
 @author: ELP
 '''
 
-
 import os,sys
-import netCDF4
-from netCDF4 import Dataset
-import csv
+#import netCDF4
+#from netCDF4 import Dataset
+import csv,time
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import pandas as pd
-import time
 import xarray as xr 
 from scipy import interpolate
 
-
 plt.style.use('bmh') 
 
-# -- define fonts
-SMALL_SIZE  = 8
-MEDIUM_SIZE = 9
-BIGGER_SIZE = 12 #9
-plt.rc('font',    size = MEDIUM_SIZE)   # controls default text sizes
-plt.rc('axes', labelsize = BIGGER_SIZE)   # fontsize of the x and y labels
+'''    The module plot Figure 5 for the 
+        Jellyfarm project, Hardangerfjord case , 2D BROM
+        Data from the Brom are plotted from start_day to stop_day,
+        For 2 columns: Baseline and "Farm"
 
-plt.rc('xtick', labelsize = BIGGER_SIZE)   # fontsize of the tick labels
-plt.rc('ytick', labelsize = BIGGER_SIZE)   # fontsize of the tick labels
-plt.rc('legend', fontsize = SMALL_SIZE)   # legend fontsize
+'''
+
+start_day = 731
+stop_day = 1096
+baseline_col = 0
+farm_col = 19
+
+# Change to True if you want to save figure 
+save = True
+
+# -- define fonts
+small_size  = 8
+medium_size = 10
+bigger_size = 13
+plt.rc('font',    size = medium_size)   # controls default text sizes
+plt.rc('axes', labelsize = bigger_size)   # fontsize of the x and y labels
+
+plt.rc('xtick', labelsize = bigger_size)   # fontsize of the tick labels
+plt.rc('ytick', labelsize = bigger_size)   # fontsize of the tick labels
+plt.rc('legend', fontsize = small_size)   # legend fontsize
 
 
 df = pd.read_csv('data/wat_hardan2.txt')
@@ -61,35 +73,22 @@ alk199 = [3070, 3050, 3590, 4040, 3800]
 tic199 = [3141.7, 3641.7, 4191.7, 4133.3, 3916.7]
 nh4199 = [100.,114.2857143,145.7142857,107.1428571,92.85714286]
 
-
-fname = (
-     r'E:\Users\EYA\Hardnew\data_Hard\BROM_Hardangerfjord_out_1X.nc')
-
-
 colr1 = '#ff8800'
 colr_base = '#2e3236'
 colr_base_field = '#519344'
 colr_farm = '#c75641'
-baseline_col = 0
-farm_col = 19
+
 a = 1
 ln  = 0.4
 ln_field = 1
 
-
-
+fname = (
+     r'E:\Users\EYA\Hardnew\data_Hard\BROM_Hardangerfjord_out_1X.nc')
 df_brom = xr.open_dataset(fname)
-
 depth_brom = df_brom['z'].values
-sed = depth_brom[15]
-sed_depth_brom  = (depth_brom - sed )*100
-time_brom = df_brom['time'].values
-len_time = len(time_brom)
-i_brom = df_brom['i'].values
-len_i = len(i_brom)
+sed_depth_brom  = (depth_brom - depth_brom[15])*100
 
 # Add oxygen 
-
 def get_df(path):
     d = pd.read_excel(path,skiprows = 1,usecols = [1,4,7],
                       names = ['depth','time','o2'])
@@ -98,29 +97,23 @@ def get_df(path):
     d.o2 = d['o2'].where(d.o2 >= 0, 0) 
     return d
     
-df_o2 = get_df('data/Jellyfarm/AKS193_1_FF.xlsx')
-df_o2_nf = get_df('data/Jellyfarm/AKS192_1_NF.xlsx')
+df_o2 =      get_df('data/Jellyfarm/AKS193_1_FF.xlsx')
+df_o2_nf =   get_df('data/Jellyfarm/AKS192_1_NF.xlsx')
 df_o2_2_nf = get_df('data/Jellyfarm/AKS189_3_NF.xlsx')
-df_o2_2 = get_df('data/Jellyfarm/AKS193_2_FF.xlsx')
-
+df_o2_2 =    get_df('data/Jellyfarm/AKS193_2_FF.xlsx')
 
 def int_and_plot(axis,xx,yy,col,col_line):
 
     f = interpolate.interp1d(yy,xx, assume_sorted = False)
     ynew = np.arange(np.min(yy),np.max(yy),0.3)
     xnew = f(ynew) 
-    
-    
-    axis.plot(xnew,ynew, zorder =5, linestyle= '--',# s = 45,
+       
+    axis.plot(xnew,ynew, zorder =5, linestyle= '--',
                      c = col_line,linewidth = ln_field,
                      alpha = 1,label = 'field "Near Farm"')
     axis.scatter(xnew,ynew, zorder = 10, s = 36, 
                      c = col,edgecolor = 'k',
                      alpha = 1,label = 'field "Near Farm"')    
-
-
-
-
 
 # plot Observations data water  
 def plot_water(var,axis,title):
@@ -141,7 +134,6 @@ def plot_water(var,axis,title):
             #        '--', color = colr_base_field, zorder = 5, linewidth = ln_field, 
             #        alpha = a)   
 
-
 # plot Observations data sediments  
 def plot_sed(var,var_depth,axis): 
     if var == None :
@@ -155,7 +147,7 @@ def plot_sed(var,var_depth,axis):
 # plot model data water          
 def plot_brom(var_brom,axis):    
     v = df_brom[var_brom].values
-    for day in range(731,1096,10):  
+    for day in range(start_day,stop_day,10):  
         axis.plot(v[day,:15,baseline_col],depth_brom[:15],
                       color = colr_base, alpha = a, 
                       linewidth = ln) 
@@ -166,7 +158,7 @@ def plot_brom(var_brom,axis):
 # plot model data sediments                             
 def plot_brom_sed(var_brom,axis): 
     v = df_brom[var_brom].values 
-    for day in range(731,1096,10):  
+    for day in range(start_day,stop_day,10):  
         axis.plot(v[day,13:,baseline_col],sed_depth_brom[13:],
                       color = colr_base, alpha = a,
                       linewidth = ln,zorder = 1 ) 
@@ -180,26 +172,18 @@ fig1.set_facecolor('white') # specify background color
 gs = gridspec.GridSpec(2, 6)
 
 gs.update(left = 0.07,right = 0.99, 
-          bottom = 0.07, top = 0.95,
-          wspace = 0.3, hspace= 0.15)
+          bottom = 0.05, top = 0.95,
+          wspace = 0.37, hspace= 0.1)
 
-ax = fig1.add_subplot(gs[0,0]) 
-ax1 = fig1.add_subplot(gs[1,0])
+def sbplt(pos): 
+    return fig1.add_subplot(pos)
 
-ax2 = fig1.add_subplot(gs[0,1]) 
-ax3 = fig1.add_subplot(gs[1,1])
-
-ax4 = fig1.add_subplot(gs[0,2]) 
-ax5 = fig1.add_subplot(gs[1,2])
-
-ax6 = fig1.add_subplot(gs[0,3])
-ax7 = fig1.add_subplot(gs[1,3]) 
-
-ax8 = fig1.add_subplot(gs[0,4])
-ax9 = fig1.add_subplot(gs[1,4])
-
-ax10 = fig1.add_subplot(gs[0,5])
-ax11 = fig1.add_subplot(gs[1,5])
+ax,ax1  =   sbplt(gs[0,0]), sbplt(gs[1,0])
+ax2,ax3 =   sbplt(gs[0,1]), sbplt(gs[1,1]) 
+ax4,ax5 =   sbplt(gs[0,2]), sbplt(gs[1,2]) 
+ax6,ax7 =   sbplt(gs[0,3]), sbplt(gs[1,3]) 
+ax8,ax9 =   sbplt(gs[0,4]), sbplt(gs[1,4])
+ax10,ax11 = sbplt(gs[0,5]), sbplt(gs[1,5])
 
 ax.set_ylabel('Depth, m')
 ax1.set_ylabel('Depth, cm')
@@ -208,15 +192,7 @@ letters = ['(A)','(B)','(C)','(D)','(E)','(F)']
 axes = (ax,ax2,ax4,ax6,ax8,ax10)
 
 [axes[n].text(-0.1, 1.05,letters[n], transform=axes[n].transAxes , 
-            size = BIGGER_SIZE) for n in range(0,len(letters))]
-'''
-#n = 0
-#for axis in (ax1,ax3,ax5,ax7,ax9,ax11): 
-#    #axis.yaxis.set_label_coords(-0.1, 0.6)
-#    axis.text(-0.2, -0.12, letters[n], transform=axis.transAxes , 
-#            size = BIGGER_SIZE) #, weight='bold')      
-#    n=n+1
-'''
+            size = bigger_size) for n in range(0,len(letters))]
 
 # plot figure with observations and model for water and sediment     
 def plot_all(var_brom,var_water,var_sed1,var_sed2,title,axis,axis1):    
@@ -242,7 +218,6 @@ def plot_all(var_brom,var_water,var_sed1,var_sed2,title,axis,axis1):
     plot_sed(var_sed2,depth199,axis1)
     axis.set_title(title)
     
-
 plot_all('O2',df.O2uM,None,None,r'$ O_2\ \mu  M $',ax,ax1) 
 plot_all('PO4',df.PO4uM,po4199,po4200,r'$ PO_4\ \mu  M $',ax2,ax3)
 plot_all('NO3',df.NO3uM,no3199,no3200,r'$ NO_3\ \mu  M $',ax4,ax5)
@@ -255,17 +230,14 @@ int_and_plot(ax1,df_o2.o2,df_o2.depth,colr_farm,colr_farm)
 int_and_plot(ax1,df_o2_nf.o2,df_o2_nf.depth,colr_base_field,colr_base_field) 
 int_and_plot(ax1,df_o2_2_nf.o2,df_o2_2_nf.depth,colr_base_field,colr_base_field) 
 
-
-
 script_dir = os.path.dirname(__file__)
 results_dir = os.path.join(script_dir, 'Results/')    
 
-save = False 
 if not os.path.isdir(results_dir):
     os.makedirs(results_dir)
 
 if save == True:    
-    plt.savefig(results_dir+'Figure1'+'.png', #'.eps'
+    plt.savefig(results_dir+'Figure5'+'.png',
                facecolor=fig1.get_facecolor(),
                 edgecolor='none')
 else: 
