1aa2d4388a8a6d878e1ff554c74251e0b91f253b
lisapro <elisaveta.pro@gmail.com>
2019-06-04 15:27:13 +0200

    new path to file

--- util.py
+++ util.py
@@ -3,16 +3,16 @@ import seaborn as sns
 import numpy as np
 
 
-path_brom1 = r'E:\Users\EYA\Hardnew\data_Hard\BROM_Hardangerfjord_out_1X.nc'
+path_brom1 = r'E:\Users\EYA\Hardnew\data_Hard\BROM_Hardangerfjord_1X.nc'
 path_brom2 = r'E:\Users\EYA\Hardnew\data_Hard\BROM_Hardangerfjord_out_2X.nc'    
 path_brom3 = r'E:\Users\EYA\Hardnew\data_Hard\BROM_Hardangerfjord_out_3X.nc'
 path_brom5 = r'E:\Users\EYA\Hardnew\data_Hard\BROM_Hardangerfjord_out_5X.nc'
-    
+
 start_day = 731
 stop_day = 1096
 dist_day = 831
 baseline_col = 0
-farm_col = 19
+farm_col = 20
 cmap = plt.get_cmap('jet')
 nlev = 30
 
